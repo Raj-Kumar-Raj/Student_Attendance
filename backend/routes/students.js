6a3dabc6cb239c8af84d948c24789f95206dbfcd
Raj Kumar Raj <143611911+Raj-Kumar-Raj@users.noreply.github.com>
2026-02-10 00:14:09 +0530

    Fix import case for Student model

--- backend/routes/students.js
+++ backend/routes/students.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const Student = require("../models/Student");
+const Student = require("../models/student");
 
 // Add student
 router.post("/", async (req, res) => {
